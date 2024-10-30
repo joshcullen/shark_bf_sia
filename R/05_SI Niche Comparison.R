b062575e286e7f5b09601cfa0000d1fd5263ff0f
Josh Cullen <joshcullen10@gmail.com>
2024-10-30 15:31:56 -0400

    Added code to compare niche size within spp by season; added inventory csv file that includes date and season per sample

--- R/05_SI Niche Comparison.R
+++ R/05_SI Niche Comparison.R
@@ -11,7 +11,10 @@ library(tictoc)
 library(ggh4x)
 
 
+#################
 ### Load data ###
+#################
+
 sia <- read.csv("Raw_data/SIA Master.csv")
 
 glimpse(sia)
@@ -22,18 +25,43 @@ sia$Species <- factor(sia$Species, level = c('Cleu','BT','BH'))
 levels(sia$Species) <- c('Bull','Blacktip','Bonnethead')
 
 
+# Load metadata to determine seasons of samples
+meta <- read_csv("Raw_data/Dock Sampling Inventory.csv")
+meta2 <- meta %>%
+  mutate(Date = gsub(pattern = "/", replacement = "-", x = Date),
+         SharkID = case_when(str_detect(ID, "^Cleu") ~ ID,
+                             TRUE ~ paste(ID, Date)),
+         TL = as.numeric(TL)) %>%
+  mutate(Date = as_date(Date, format = "%m-%d-%y"),
+         month = month(Date),
+         season = case_when(month %in% c(12, 1, 2) ~ 'Winter',
+                            month %in% c(3:5) ~ 'Spring',
+                            month %in% c(6:8) ~ 'Summer',
+                            month %in% c(9:11) ~ 'Fall'),
+         .after = Date)
+
+# Add 'month' and 'season' to SIA data
+sia2 <- left_join(sia,
+                  meta2[,c("SharkID","TL","month","season")],
+                  by = join_by("SharkID","TL"))
+
+
+
+##############################
 ### Clean and wrangle data ###
+##############################
 
 #Check for duplicates and remove record(s) for any duplicate IDs
-ind <- which(duplicated(sia$SharkID) | is.na(sia$TL))
-sia <- sia[-ind,]
+ind <- which(duplicated(sia2$SharkID) | is.na(sia2$TL))
+sia2 <- sia2[-ind,]
 
 
-
+#######################################
 ### Calculate summary niche metrics ###
+#######################################
 
 # Mean and SD of d13C and d15N by spp
-sia %>%
+sia2 %>%
   group_by(Species) %>%
   summarize(mean.C = mean(d13C),
             sd.C = sd(d13C),
@@ -42,16 +70,42 @@ sia %>%
             n = n())
 
 
+# Mean and SD of d13C and d15N by spp and season
+sia2 %>%
+  group_by(Species, season) %>%
+  summarize(mean.C = mean(d13C),
+            sd.C = sd(d13C),
+            mean.N = mean(d15N),
+            sd.N = sd(d15N),
+            n = n())
 
 
+#######################################################
+### Explore size distribution by species and season ###
+#######################################################
+
+sia2 %>%
+  mutate(season = factor(season, levels = c('Spring','Summer','Fall'))) %>%
+  drop_na(season) %>%
+
+ggplot(aes(TL)) +
+  geom_density(aes(fill = Species)) +
+  theme_bw(base_size = 14) +
+  facet_grid(season ~ Species, scales = "free_x")
+
+
+
+######################################
 ### Calculate isotopic niche width ###
+######################################
 
+### For all samples per spp
 # generate parameter draws from the "default" posteriors of each spp
-sia2 <- sia[,c('d13C','d15N','Species')]
+sia3 <- sia2[,c('d13C','d15N','Species','season')]
 
 set.seed(2024)
-sia.par <- tapply(1:nrow(sia2), sia2$Species,
-                  function(ii) niw.post(nsamples = 5e3, X = sia2[ii,1:2]))
+sia.par <- tapply(1:nrow(sia3), sia3$Species,
+                  function(ii) niw.post(nsamples = 5e3, X = sia3[ii,1:2]))
 
 niche.par.plot(sia.par, col = met.brewer('Egypt', n = 3), plot.mu = TRUE, plot.Sigma = TRUE)
 legend("topright", legend = names(sia.par), fill = met.brewer('Egypt', n = 3))
@@ -59,9 +113,9 @@ legend("topright", legend = names(sia.par), fill = met.brewer('Egypt', n = 3))
 
 
 # Viz estimated niche regions
-tmp <- tapply(1:nrow(sia2), sia2$Species,
-                  function(ii) niw.post(nsamples = 50, X = sia2[ii,1:2]))
-sia.data <- tapply(1:nrow(sia2), sia2$Species, function(ii) X = sia2[ii,1:2])
+tmp <- tapply(1:nrow(sia3), sia3$Species,
+                  function(ii) niw.post(nsamples = 50, X = sia3[ii,1:2]))
+sia.data <- tapply(1:nrow(sia3), sia3$Species, function(ii) X = sia3[ii,1:2])
 
 niche.plot(niche.par = tmp, niche.data = sia.data, pfrac = .05, alpha = 0.95,
            iso.names = expression(delta^{13}*C, delta^{15}*N),
@@ -69,13 +123,22 @@ niche.plot(niche.par = tmp, niche.data = sia.data, pfrac = .05, alpha = 0.95,
 
 
 
+### For each spp x season grouping
+# generate parameter draws from the "default" posteriors of each spp x season
+sia4 <- sia3 %>%
+  drop_na(season)
+
+set.seed(2024)
+sia.par.szn <- tapply(1:nrow(sia4), paste(sia4$Species, sia4$season, sep = "_"),
+                      function(ii) niw.post(nsamples = 5e3, X = sia4[ii,1:2]))
+
 
 
 #####################################
 ### Create figures for manuscript ###
 #####################################
 
-### Plot subset of Bayesian posterior ellipses ###
+### Plot subset of Bayesian posterior ellipses per species ###
 
 n.post <- 50  #number of posterior draws
 
@@ -172,6 +235,122 @@ p.cn / p.nw +
         plot.tag = element_text(size = 18, hjust = 0, vjust = 0))
 
 ggsave("Figures/Figure 4.tiff", width = 6, height = 8, units = "in", dpi = 400)
+
+
+
+
+
+
+
+### Plot subset of Bayesian posterior ellipses per species x season ###
+
+n.post <- 50  #number of posterior draws
+
+# define empty list to store resulting ellipses
+all_ellipses_szn <- vector("list", length = length(sia.par.szn)) |>
+  set_names(names(sia.par.szn))
+
+# generate ellipses from posterior
+for (i in seq_along(sia.par.szn)) {
+
+  ell = NULL
+
+  for (j in 1:n.post) {
+    out <- nicheROVER::ellipse(mu = sia.par.szn[[i]]$mu[j,],
+                               sia.par.szn[[i]]$Sigma[,,j],
+                               alpha = 0.95,
+                               n = 100)
+    out2 <- cbind(rep = j, out)
+
+    ell <- rbind(ell, out2)
+  }
+
+  all_ellipses_szn[[i]] <- data.frame(ell)
+}
+
+
+# Merge all species together
+ellipse_df_szn <- all_ellipses_szn |>
+  bind_rows(.id = "group") |>
+  separate(col = "group", into = c("Species",'season')) |>
+  rename(d13C = x, d15N = y) |>
+  mutate(Species = factor(Species, levels = c('Bull','Blacktip','Bonnethead')),
+         season = factor(season, levels = c('Spring','Summer','Fall')))
+
+
+# Create plot of ellipses
+p.cn.szn <- ggplot() +
+  geom_point(data = sia4, aes(d13C, d15N, color = Species), size = 2, alpha = 0.7) +
+  geom_path(data = ellipse_df_szn, aes(d13C, d15N, group = interaction(rep, Species),
+                                       color = Species), alpha = 0.15) +
+  scale_color_met_d("Egypt") +
+  labs(x = expression(paste(delta^{13}, "C (\u2030)")), y = expression(paste(delta^{15}, "N (\u2030)"))) +
+  theme_bw() +
+  theme(axis.text = element_text(size=16),
+        axis.title = element_text(size=18),
+        panel.grid = element_blank()) +
+  # guides(color = "none") +
+  facet_wrap(~ season, nrow = 2)
+
+
+
+### Plot niche width ###
+
+# posterior distribution of niche size by species
+niche.width.szn <- sapply(sia.par.szn, function(spec) {
+  apply(spec$Sigma, 3, niche.size, alpha = 0.95)
+})
+
+# point estimate and standard error
+rbind(est = colMeans(niche.width.szn),
+      se = apply(niche.width.szn, 2, sd))
+
+
+# convert niche width into long-format data.frame
+niche.width.szn.df <- data.frame(niche.width.szn) |>
+  mutate(tmp = 1:n()) |>
+  pivot_longer(cols = -tmp, names_to = "group", values_to = "niche_width") |>
+  separate(col = "group", into = c("Species",'season')) |>
+  select(-tmp) |>
+  mutate(Species = factor(Species, levels = c('Bull','Blacktip','Bonnethead')),
+         season = factor(season, levels = c('Spring','Summer','Fall')))
+
+
+p.nw.szn <- ggplot(niche.width.szn.df, aes(Species, niche_width)) +
+  ggdist::stat_halfeye(aes(fill = Species), adjust = 0.5, width = 0.6, .width = 0,
+                       justification = -0.3, point_color = NA) +
+  scale_fill_met_d("Egypt") +
+  geom_jitter(aes(color = Species), width = .05, alpha = .05) +
+  scale_color_met_d("Egypt") +
+  geom_boxplot(width = 0.2, outlier.shape = NA, fill = "transparent") +
+  # scale_y_continuous(breaks = seq(0, 14, by = 2)) +
+  labs(x = "", y = expression("Ellipse Area " ('\u2030' ^2) )) +
+  theme_bw() +
+  theme(panel.grid = element_blank(),
+        axis.title = element_text(size = 18),
+        axis.text = element_text(size = 14),
+        legend.title = element_blank(),
+        legend.position = c(0.85, 0.85),
+        legend.text = element_text(size = 12)) +
+  facet_wrap(~ season, nrow = 2)
+
+
+
+
+
+### Create composite plot ###
+
+p.cn.szn / p.nw.szn +
+  plot_layout(guides = "collect") +
+  plot_annotation(tag_levels = 'A', tag_prefix = "(", tag_suffix = ")") &
+  theme(legend.position = 'top',
+        # plot.tag.position = c(0.09, 1),
+        plot.tag = element_text(size = 18, hjust = 0, vjust = 0))
+
+ggsave("Figures/Figure S2.tiff", width = 6, height = 8, units = "in", dpi = 400)
+
+
+
 
 
 
